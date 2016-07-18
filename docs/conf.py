329cea5a0ff80fdcf4dd79ca4558ea65f641859b
Changaco <changaco@changaco.oy.lc>
2016-07-18 19:46:16 +0200

    bump version to 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'Chad Whitacre et al.'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '1.0.0-dev'
+release = '1.1.0'
 # The short X.Y version.
 version = '.'.join(release.split('.')[:2])
 
