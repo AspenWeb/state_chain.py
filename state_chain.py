5947c24244e7e80298d8a182b7c3667393b6a27d
Changaco <changaco@changaco.oy.lc>
2018-06-22 11:56:53 +0200

    bump version to 1.3.0

--- state_chain.py
+++ state_chain.py
@@ -162,7 +162,7 @@ import types
 from dependency_injection import get_signature, resolve_dependencies
 
 
-__version__ = '1.2.0-dev'
+__version__ = '1.3.0'
 PYTHON_2 = sys.version_info < (3, 0, 0)
 
 
